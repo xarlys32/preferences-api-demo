d30cbfa3ab5fc578e602ab9629c0f9366395028d
xarlys32 <57732203+xarlys32@users.noreply.github.com>
2025-03-22 20:03:38 +0100

    Updates

---
+++ src/test/java/com/vw/preferences/domain/usecase/event/EventHistoryUseCasesTest.java
@@ -0,0 +1,8 @@
+package com.vw.preferences.domain.usecase.event;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class EventHistoryUseCasesTest {
+}
